9cef2600344f6f47768650073cf178cdbaed27cf
jackso88 <67680872+jackso88@users.noreply.github.com>
2023-01-20 17:47:35 +0300

    Revert "Dev"

--- docker/backend/worker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-until cd /app/backend
-do
-    echo "Waiting for server volume..."
-done
-
-celery -A backend worker --loglevel=info --concurrency 1 -E
--- frontend/src/components/Redirect/Redirect.js
+++
@@ -1,9 +0,0 @@
-function Redirect(){
-    window.location.replace(window.location.origin+'/login/');
-    return(
-        <>
-        </>
-    )
-}
-
-export default Redirect
